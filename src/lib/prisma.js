cbc61e4b5915bb9536028e8bebdd671eec2ef445
piecahcih <pichayapa.thai@gmail.com>
2026-05-07 00:15:36 +0700

    change prisma lib to deploy

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,11 +2,21 @@ import 'dotenv/config';
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client.js";
 
+// const adapter = new PrismaMariaDb({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE_NAME,
+//     connectionLimit: 5
+// })
+const databaseUrl = new URL(process.env.DATABASE_URL);
+
 const adapter = new PrismaMariaDb({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: databaseUrl.hostname,
+    port: parseInt(databaseUrl.port) || 3306,
+    user: decodeURIComponent(databaseUrl.username),
+    password: decodeURIComponent(databaseUrl.password),
+    database: databaseUrl.pathname.slice(1),
     connectionLimit: 5
 })
 
