b64f09bc207cc38c665e979dad535b447998f5bf
piecahcih <pichayapa.thai@gmail.com>
2026-05-06 22:40:52 +0700

    tried to deploy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import bookRoute from './routes/book.route.js'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5174"],
+    origin: ["http://localhost:5173"],
     methods: ["GET","POST","PUT","PATCH","DELETE"],
     credentials: true
 }))
